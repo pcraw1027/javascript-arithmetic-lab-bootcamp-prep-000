bfbe2e8151c4415f3d5f81d9e95ad849f82329d6
Peter Crawford <pcraw1027@gmail.com>
2021-01-27 13:20:45 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function increment(number) {
+  return nunber += 1
+}
+
+function decrement(number) {
+  return number -= 1
+}
+
+function makeInt(string) {
+  return parseInt(string,10)
+}
+
+function preserveDecimal(string) {
+  return parseFloat(string)
+}
